05d4f2f066a7ea09f60e687985ee01d44639d6bf
Richardson902 <160741608+Richardson902@users.noreply.github.com>
2025-02-20 14:50:29 -0400

    imported tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import dns from "node:dns";
 
@@ -6,7 +7,7 @@ dns.setDefaultResultOrder("verbatim");
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   server: {
     proxy: {
       "/api/": {
